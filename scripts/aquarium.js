1e56d6e4395c144db2561b09f7422e64e7d4dde2
Arthur <art1choke@yandex.com>
2025-05-25 21:17:10 +0500

    size

--- scripts/aquarium.js
+++ scripts/aquarium.js
@@ -18,7 +18,7 @@ let active = false;
 const colors = ['black', 'white'];
 const animaSpeed = window.innerWidth <= 768 ? 1.5 : 5;
 const minSize = window.innerWidth <= 768 ? 3 : 10;
-const maxSize = window.innerWidth <= 768 ? 15 : 30;
+const maxSize = window.innerWidth <= 768 ? 8 : 30;
 
 function Circle(x, y, radius) {
     this.x = x ;

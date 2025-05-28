f37c83220069bdd6737949dc0a95dbe101959389
Arthur <art1choke@yandex.com>
2025-05-28 17:55:15 +0500

    size

--- games/Snake/game.js
+++ games/Snake/game.js
@@ -6,7 +6,7 @@ const overlay = document.getElementById('overlay');
 const size = 25;
 
 const rows = Math.floor((window.innerWidth - 50)/25);
-const colls = Math.floor((window.innerHeight - 1000)/25);
+const colls = Math.floor((window.innerHeight - 3000)/25);
 canvas.width = rows * size;
 canvas.height = colls * size;
 
